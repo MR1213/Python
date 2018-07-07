e1b5b1dbefcdb8a73bae98a0ba83760f038b86e2
Maksym <maxsusha@gmail.com>
2018-07-07 16:12:10 -0700

    cube

--- graphs/square2.py
+++ graphs/square2.py
@@ -1,10 +1,11 @@
 import turtle
 
 turtle.shape("turtle")
-
+turtle.hideturtle()
+turtle.ht()
 turtle.forward(25)
-
-x = 100
+turtle.speed(0)
+x = 90
 a = 90
 
 for i in range(1, 100):
@@ -18,6 +19,7 @@ for i in range(1, 100):
     turtle.forward(x)
     turtle.left(a)
 
-    turtle.left(7)
+    turtle.left(123)
 
 turtle.exitonclick()
+turtle.ht()
