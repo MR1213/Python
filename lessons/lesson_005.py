bc3b122718f189f4561d1566e19c7d8296591a49
Maksym <maxsusha@gmail.com>
2019-01-14 21:14:30 -0800

    Made a program that checks if a number is even or odd.

---
+++ lessons/lesson_005.py
@@ -0,0 +1,15 @@
+num = int(input("Give me a number to check: "))
+check = int(input("Give me a number to divide by: "))
+ans= num/check
+if num % 4 == 0:
+    print(num, "Is a multiple of 4")
+elif num % 2 == 0:
+    print(num, "Is an even number")
+else:
+    print(num, "Is an odd number")
+
+if num % check == 0:
+    print(num, "Divides evenly by", check)
+    print(str(num) + " divided by " + str(check) + " is " + str(ans))
+else:
+    print(num, "Does not divide evenly by", check)
