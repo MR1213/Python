1c3ff83f89b96f461af632395ddeef21c99ced44
sergeyltd <sergeyltd@gmail.com>
2019-08-07 21:34:41 -0700

    Update lesson_010.py

--- lessons/lesson_010.py
+++ lessons/lesson_010.py
@@ -1,17 +1,17 @@
 import turtle
 turtle.shape("turtle")
 turtle.hideturtle()
-turtle.ht()
+#turtle.ht()
 turtle.forward(0)
 turtle.speed(0)
 x = 100
-a = 1
+a = 101
 
 for i in range(1, 2500):
 
     turtle.forward(x)
     turtle.left(a)
-    turtle.left(100)
+    #turtle.left(100)
 
 turtle.exitonclick()
-turtle.ht()
+#turtle.ht()
