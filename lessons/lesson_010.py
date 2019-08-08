041c743eada004584a493f89c0d7cc0177d28729
Maksym <maxsusha@gmail.com>
2019-08-08 15:00:08 -0700

    learned how to fill shapes

--- lessons/lesson_010.py
+++ lessons/lesson_010.py
@@ -4,14 +4,14 @@ turtle.hideturtle()
 turtle.ht()
 turtle.forward(0)
 turtle.speed(0)
-x = 100
-a = 1
+x = 30
+a = 100
 
-for i in range(1, 2500):
+for i in range(1, 250):
 
     turtle.forward(x)
     turtle.left(a)
-    turtle.left(100)
+    turtle.left(200)
 
 turtle.exitonclick()
 turtle.ht()
