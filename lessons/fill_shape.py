e25f29311d5ea1e46b2720d85616dc694c215d44
Maksym <maxsusha@gmail.com>
2019-08-08 14:59:07 -0700

    upgraded

---
+++ lessons/fill_shape.py
@@ -0,0 +1,22 @@
+import turtle
+a=6
+b=25
+c=75
+d=15
+turtle.speed(0)
+turtle.shape("turtle")
+turtle.fillcolor('blue')
+turtle.begin_fill()
+for i in range(a):
+    turtle.forward(b)
+    turtle.right(c)
+    turtle.left(d)
+turtle.end_fill()
+turtle.ht()
+turtle.exitonclick()
+
+
+#a-360 b-1 c-1 : circle
+#a-4 b-any number c-90 : square
+#a-3 b-any number c-120 : triangle
+#a=12 b-any number c-45 : hexagon
